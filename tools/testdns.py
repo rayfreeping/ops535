a56a155d69585ac9f03feea7f713706930466994
Raymond Chan <rayfree.ping@gmail.com>
2021-03-03 22:36:28 -0500

    update testdns.py script

--- tools/testdns.py
+++ tools/testdns.py
@@ -1,50 +1,58 @@
 #! /usr/bin/python
 # ops535 tools for testing DNS servers
-# caching: 192.168.x.153
-# primary: 192.168.x.53
-# root: 192.168.x.253
+# caching: 192.168.x.3
+# primary: 192.168.x.2
+# root: 192.168.x.4
 
 import dns.resolver
 
-network_id = raw_input("Your Network ID (1-32): ")
-domain_name = raw_input("Your Domain name: ")
+network_id = input("Your Network ID (1-43): ")
+domain_name = input("Your Domain name: ")
 
-pri_dns = '.'.join(['192','168',network_id,'53'])
-co_dns = '.'.join(['192','168',network_id,'153'])
-rns_dns = '.'.join(['192','168',network_id,'253'])
+pri_dns = '.'.join(['192','168',network_id,'2'])
+co_dns = '.'.join(['192','168',network_id,'3'])
+rns_dns = '.'.join(['192','168',network_id,'4'])
 
-print "Testing your DNS server, IP ",pri_dns
-print "And your domain name is ", domain_name
+print("Testing your DNS server, IP ",pri_dns)
+print("And your domain name is ", domain_name)
 
-dummy = raw_input("Press ENTER when ready:")
+dummy = input("Press ENTER when ready:")
 
 # fqdn to be tested:
-# sdn.[domain-name] 192.168.x.33
-# neutron.[domain-name] 192.168.x.66
-#
-# hosts=['sdn','neutron']
-# a records: a_rec=".".join([hosts[0],domain_name])
-#
+# pri-dns.[domain-name] 192.168.x.2
+# co-nfs.[domain-name] 192.168.x.3
+# rns-ldap.[domain-name] 192.168.x.4
+
+host_list = ['pri-dns','co-nfs','rns-ldap']
+ip_address = ['192.168.2.2','192.168.2.3','192.168.2.4']
+
+fqdn_s = []
+for host in host_list:
+    fqdn_s.append(".".join([host,domain_name]))
+
 myResolver = dns.resolver.Resolver()
+# testing primary dns server 
+
 myResolver.nameservers=[pri_dns]
 try:
-	myAnswers=myResolver.query("cp.net","SOA")
-	for rdata in myAnswers:
-		print rdata
-		print "email: ",rdata.rname
-		print "server: ",rdata.mname
-		print "serial no: ",rdata.serial
+    myAnswers=myResolver.query(domain_name,"SOA")
+    for rdata in myAnswers:
+        print(rdata)
+        print("email: ",rdata.rname)
+        print("server: ",rdata.mname)
+        print("serial no: ",rdata.serial)
 
 except:
-	print "SOA Query failed."
+    print("SOA Query failed.")
 
-try:
-	myAnswers = myResolver.query("pri.cp.net","A")
-	for rdata in myAnswers:
-		print rdata
+for fqdn in fqdn_s:
+    try:
+        myAnswers = myResolver.query(fqdn,"A")
+        print('Query result for:',fqdn)
+        for rdata in myAnswers:
+            print('Answer:',rdata)
 
-except:
-	print "Adderss query for pri failed."
-
+    except:
+        print("Adderss query for',fqdn,'failed.")
 
 # myResolver.read_resolv_conf("/etc/resolv.conf") <-- restore nameserver IP
